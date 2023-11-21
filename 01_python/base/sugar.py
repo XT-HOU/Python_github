b00708c98c86f84204a0abd9952b931362c77cc5
HOUXT <1107440650@qq.com>
2023-11-21 16:08:19 +0800

    python 基础

--- 01_python/base/sugar.py
+++ 01_python/base/sugar.py
@@ -12,3 +12,18 @@ if all(conditions):
 
 if any(conditions):
     print("Yes, I want to but the product.")
+
+# 元组拆包，站位符“-”
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+for country, _ in traveler_ids:
+    print(country)
+# 元组拆包，用*处理 剩下的元素
+a, b, *rest = range(5)
+print(a, b, rest)
+# 符号 +=，*=
+l = [1, 2, 3]
+print(id(l))
+l *= 2
+print(id(l))
+l += [9]
+print(id(l))
